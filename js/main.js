3e34d9b4e9ad495306a39c3462476b4a591f798b
Libertad Chapinal <libertadchapinalcervantes@gmail.com>
2019-04-18 14:43:48 +0200

    List of news

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 const data = [
   {
     title: 'Asteroids 101',
@@ -22,3 +23,29 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+
+//Create structure (elements)
+//Create content
+
+for(let i = 0; i<data.length; i++){
+  const news = document.querySelector('news');
+
+  const list = document.createElement('li');
+  list.classList.add('news__item');
+
+  const title = document.createElement('h2');
+  title.classList.add('news__title');
+  const contentTitle = document.createTextNode(data[i].title);
+  title.appendChild(contentTitle);
+
+  const image = document.createElement('img');
+  image.classList.add('news__image');
+  const contentImg = document.createTextNode(data[i].image);
+  image.appendChild(contentImg);
+
+  list.appendChild(title);
+  list.appendChild(image);
+
+  //Esto no funciona, por qué???
+  news.appendChild(list);
+}
